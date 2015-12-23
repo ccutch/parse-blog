fe50ba2dceeb831107eef677b49f1239625b41c6
Connor McCutcheon <connormccutcheon95@gmail.com>
2015-12-23 00:09:50 -0800

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,24 +1,5 @@
 /* @flow */
 import Parse from "parse"
-
-Parse.initialize("eRlLRSHvJ0qjk2kikfPde9jhF0wUEu7IcXFS4KJe", "aOB7yQ8q8LK5agrAej8TrBVn9KxqxI1ra70jBm7R");
-
-
-function createUser(username: string, password: string, email: string) {
-  const user = new Parse.User()
-  user.set("username", username)
-  user.set("password", password)
-  user.set("email", email)
-
-  return user.signUp(null)
-}
-
-console.log(Parse.User.current())
-console.log(Parse.Session)
-
-
-
-// React time
 import React from "react"
 import ReactDOM from "react-dom"
 import styles from "./styles/application.css"
