dd44013261217dd7cf334fdc6e7eb03da9e16148
Connor McCutcheon <connormccutcheon95@gmail.com>
2015-12-25 23:12:01 -0800

    adding keys

---
+++ app/config/keys.js
@@ -0,0 +1,5 @@
+
+
+export const parse_application_id = "RCiPPDCtwzxToUsCRLe9MvMhbV0R6QlHrbxh0Unw"
+export const parse_client_key = "ybLymQ22TJabo23IQCP1XhIzTMvT1A1cXj9YUAD0"
+export const parse_javascript_key = "ozj6fGc7RG8wmFoU2HN8RlX0rznjiEgiOj3Bm6NL"
